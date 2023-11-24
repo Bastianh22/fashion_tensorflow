0284bc5bd3efd3bdd4a40c3e743134fa8eb8b3a8
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2023-11-24 15:23:49 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import keras 
+from keras import layers
+import matplotlib.pyplot as plt
+
+import os.path as path
+
+class_names = ['T-shirt', 'pantalon', 'pull', 'robe', 'manteau', 'sandale', 'chemise', 'basket', 'sac', 'bottine']
+
+mnist = keras.datasets.fashion_mnist
+
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+
+if (path.isfile("fashion_model.keras")):
+  print("hello")
+  model = keras.models.load_model("fashion_model.keras")
+else :
+  model = keras.models.Sequential([
+    layers.Flatten(), 
+    layers.Dense(128, activation="relu"), 
+    layers.Dense(10, activation="softmax")
+  ])
+
+model.compile(
+  optimizer = "adam",
+  loss = 'sparse_categorical_crossentropy',
+  metrics=['accuracy']
+)
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+model.save("fashion_model.keras")
+
+classifications = model.predict(test_images)
+print(classifications[8])
+print(class_names[test_labels[8]])
